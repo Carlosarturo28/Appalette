531a1609bc210dfcc1a2956f6247d57930efbfd2
Carlos Navarro <carlosarturo28@gmail.com>
2022-11-25 12:40:40 +0100

    updating database

---
+++ src/database.js
@@ -0,0 +1,12 @@
+export const data = [
+  {
+    "name": "Instagram",
+    "colors": ["#2D424E", "#AFAF4A", "#FBAE9A", "#000000", "#FFFFFF"],
+    "logo": "https://lookaside.fbsbx.com/elementpath/media/?media_id=676073767417807&version=1668818186"
+  },
+  {
+    "name": "Facebook",
+    "colors": ["#F3A18A", "#F9AB4B", "#FBDD9A"],
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/600px-Facebook_Logo_%282019%29.png"
+  }
+]
