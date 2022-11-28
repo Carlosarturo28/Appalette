ed5fe9519e1f4426413bfb31a79d94f65b2be1f8
Carlos Navarro <carlosarturo28@gmail.com>
2022-11-29 00:26:27 +0100

    updating db and adding css

--- src/ui.tsx
+++ src/ui.tsx
@@ -7,64 +7,85 @@ import { BrandItem } from "./components/BrandItem/index";
 declare function require(path: string): any;
 
 const App = () => {
+  const [data, setData] = React.useState([]);
+  // Function called when a result is clicked
+  const chooseBrand = (brand) => {
+    const numberOfColors = brand.colors.length;
+    const colors = brand.colors;
+    const brandName = brand.name;
+    parent.postMessage(
+      { pluginMessage: { colors, numberOfColors, brandName } },
+      "*"
+    );
+  };
 
-  const [data, setData] = React.useState([])
-// Function called when a result is clicked
-const chooseBrand = (brand) => {
-  const numberOfColors = brand.colors.length;
-  const colors = brand.colors;
-  const brandName = brand.name
-  parent.postMessage({ pluginMessage: { colors, numberOfColors, brandName } }, "*");
-};
-
-const filterBrand = (brands, query) => {
-  if (!query) {
+  const filterBrand = (brands, query) => {
+    if (!query) {
       return [];
-  }
+    }
 
-  return brands.filter((post) => {
+    return brands.filter((post) => {
       const postName = post.name.toLowerCase();
       return postName.includes(query);
-  });
-};
-const [searchQuery, setSearchQuery] = React.useState('');
-const filteredBrand = filterBrand(data, searchQuery.toLowerCase());
+    });
+  };
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const filteredBrand = filterBrand(data, searchQuery.toLowerCase());
 
-const fetchData = async () => {
-  fetch('https://raw.githubusercontent.com/Carlosarturo28/Appalette/master/src/database.json').then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error('Something went wrong');
-  })
-  .then((data) => {
-    setData(data)
-  })
-  .catch((error) => {
-    console.log(error)
-  });
-}
+  const fetchData = async () => {
+    fetch(
+      "https://raw.githubusercontent.com/Carlosarturo28/Appalette/master/src/database.json"
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Something went wrong");
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-React.useEffect( () => {
-  fetchData()
-}, [])
+  React.useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="container">
-      <Searchbar
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      />
+      <Searchbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <ul>
-        {filteredBrand.length > 0
-          ? filteredBrand.map((brand) => (
-            <BrandItem key={Math.random()} onClick={chooseBrand} brand={brand} />
+        {searchQuery.length > 0 && filteredBrand.length === 0 ? (
+          <>
+            <img src={require("./assets/no-results.png")} width="150" />
+            <p className="tweet">
+              Didn't find the App?{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://twitter.com/intent/tweet?text=Hey%20@cartur28,%20Appalette%20is%20missing%20this%20app:"
+              >
+                Send me a tweet!
+              </a>
+            </p>
+          </>
+        ) : searchQuery.length === 0 ? (
+          <img src={require("./assets/empty.png")} width="250" />
+        ) : (
+          filteredBrand.map((brand) => (
+            <BrandItem
+              key={Math.random()}
+              onClick={chooseBrand}
+              brand={brand}
+            />
           ))
-          : <img src={require('./assets/empty.png')} width="200" />
-        }
+        )}
       </ul>
     </div>
   );
-}
+};
 
 ReactDOM.render(<App />, document.getElementById("react-page"));
